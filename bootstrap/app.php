a5850bee79a81329e0881c5c6fffc7b0165e0456
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2024-06-05 12:00:18 +0100

    Update project setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,15 +12,15 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
-
-        $middleware->alias([
-            'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
+        $middleware->web(append: [
+            \App\Http\Middleware\HandleInertiaRequests::class,
+            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
         //
+         $middleware->validateCsrfTokens(except: [
+            'http://127.0.0.1:8000/*',
+    ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
